f6e0bb3b84ecef743ef57d29b34886cd57d7c672
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-01-08 11:39:29 -0600

    add more methods

--- destinations-mongoose.js
+++ destinations-mongoose.js
@@ -1,10 +1,17 @@
+//grab the mongoose package
 const mongoose = require("mongoose")
+
+//connection string to a mongodb running locally
+// normally you should get this from an .env file
 const mongoURI = "mongodb://localhost:27017/travelr"
 
+//connect to the database
+//again in production apps don't print this uri
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
-    console.log('connected to mongodb')
+    console.log(`connected to mongodb ${mongoURI}`)
 })
 
+//define the schema our app will use to connect to this database
 const destinationSchema = new mongoose.Schema({
     city: {type: String, required: true},
     country: String,
@@ -18,6 +25,7 @@ const destinationSchema = new mongoose.Schema({
 //connect our database collection using this schema
 const DestinationModel = mongoose.model('destinations', destinationSchema)
 
+//some .find queries
 async function getDestinations() {
     const destinations = await DestinationModel.find()
     console.log("destinations", destinations)
